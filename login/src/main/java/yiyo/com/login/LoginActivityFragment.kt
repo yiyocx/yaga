6f3f5eb13b43978f9d39e88dd479c43f155cd752
Yiyo Castillo <yiyocx@gmail.com>
2019-06-14 00:11:16 -0500

    Created login module with basic fragment

---
+++ login/src/main/java/yiyo/com/login/LoginActivityFragment.kt
@@ -0,0 +1,20 @@
+package yiyo.com.login
+
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class LoginActivityFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
