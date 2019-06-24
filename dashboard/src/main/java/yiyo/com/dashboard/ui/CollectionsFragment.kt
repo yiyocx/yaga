b22e97f21e5a426fb1d3d841a7833cb39649c576
Yiyo Castillo <yiyocx@gmail.com>
2019-06-24 11:02:05 -0500

    Changes to dashboard nav graph

--- dashboard/src/main/java/yiyo/com/dashboard/ui/CollectionsFragment.kt
+++ dashboard/src/main/java/yiyo/com/dashboard/ui/CollectionsFragment.kt
@@ -1,4 +1,4 @@
-package yiyo.com.dashboard
+package yiyo.com.dashboard.ui
 
 
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import yiyo.com.dashboard.R
 
 class CollectionsFragment : Fragment() {
 
--- dashboard/src/main/java/yiyo/com/dashboard/ui/PhotosFragment.kt
+++ dashboard/src/main/java/yiyo/com/dashboard/ui/PhotosFragment.kt
@@ -1,4 +1,4 @@
-package yiyo.com.dashboard
+package yiyo.com.dashboard.ui
 
 
 import android.os.Bundle
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import yiyo.com.dashboard.R
 
 class PhotosFragment : Fragment() {
 
